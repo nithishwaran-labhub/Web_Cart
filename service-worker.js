a99682b200ff7c58208e55d7916b00b944834517
nithishwaran-labhub <nithishwaran321io@gmail.com>
2026-01-22 19:26:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,28 @@
+const cacheName = "webcart-v1";
+
+const filesToCache = [
+  "index.html",
+  "cart.html",
+  "checkout.html",
+  "wishlist.html",
+  "login.html",
+  "signup.html",
+  "style.css",
+  "script.js",
+  "cart.js",
+  "wishlist.js",
+  "logo.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
